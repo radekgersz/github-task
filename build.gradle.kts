e094b728ee8efc1c1614f29ca79b77fb136fc175
radekgersz <radekgersz@gmail.com>
2026-06-17 13:50:05 +0200

    first commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,18 @@
 plugins {
     id("java")
+    // 1. Adds Spring Boot capabilities
+    id("org.springframework.boot") version "4.0.0"
+    // 2. Manages version numbers for all Spring dependencies automatically
+    id("io.spring.dependency-management") version "1.1.5"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+// 3. Explicitly sets the Java version to 25 as requested
+java {
+    sourceCompatibility = JavaVersion.VERSION_25
+}
 
 repositories {
     mavenCentral()
